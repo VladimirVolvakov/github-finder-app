a8104b3353fc9a7876234242f6e8e55fc20d92f2
VladimirVolvakov <dr.volvakov@gmail.com>
2021-12-23 14:11:49 +0300

    Done primary layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import Footer from './components/layout/Footer'
+import Home from './pages/Home'
+import Navbar from './components/layout/Navbar'
+import NotFound from './pages/NotFound'
+
 function App() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className='flex flex-col justify-between h-screen'>
+        <Navbar />
+
+        <main className='container mx-auto px-3 pb-12'>
+          <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route exact path='/about' element={<About />} />
+            <Route path='/notfound' element={<NotFound />} />
+            <Route path='/*' element={<NotFound />} />
+          </Routes>
+        </main>
+
+        <Footer />
+      </div>
+      
+    </Router>
   );
 }
 
