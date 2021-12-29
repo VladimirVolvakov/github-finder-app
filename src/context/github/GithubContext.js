c92c78d1a64306de8dacddc2499f747ca366b0be
VladimirVolvakov <dr.volvakov@gmail.com>
2021-12-29 20:57:55 +0300

    add first reducer

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,33 +1,45 @@
-import { createContext, useState } from 'react'
+import { createContext, useReducer } from 'react'
+import githubReducers from './GithubReducers'
 
 const GithubContext = createContext()
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN
 
-
 export const GithubProvider = ({ children }) => {
-    const [users, setUsers] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+  const initialState = {
+    users: [],
+    isLoading: true,
+  }
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`, {
-          headers: {
-            Authorization: `token ${GITHUB_TOKEN}`,
-          },
-        })
-    
-        const data = await response.json()
-    
-        setUsers(data)
-        setIsLoading(false)
-    }
+  const [state, dispatch] = useReducer(githubReducers, initialState);
 
-    return <GithubContext.Provider value={{
-        users,
-        isLoading,
-        fetchUsers
-    }}>{ children }</GithubContext.Provider>
+  const fetchUsers = async () => {
+    const response = await fetch(`${GITHUB_URL}/users`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    })
+
+    const data = await response.json()
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    })
+  }
+
+  return (
+    <GithubContext.Provider
+      value={{
+        users: state.users,
+        isLoading: state.isLoading,
+        fetchUsers,
+      }}
+    >
+      {children}
+    </GithubContext.Provider>
+  )
 }
 
 export default GithubContext
---
+++ src/context/github/GithubReducers.js
@@ -0,0 +1,13 @@
+const githubReducers = (state, action) => {
+    switch (action.type) {
+        case 'GET_USERS':
+            return {
+                ...state, 
+                users: action.payload,
+                isLoading: false
+            }
+        default: return state
+    }
+}
+
+export default githubReducers
